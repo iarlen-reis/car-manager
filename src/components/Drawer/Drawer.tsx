80d55fc06e81458bcd5504bef15a87e80d247d98
Iarlen <iarlensr@hotmail.com>
2023-06-16 19:14:13 -0300

    refactor: refactoring Drawer component and modifying styling.

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,19 +1,26 @@
 'use client'
 import React from 'react'
 import {
-  Drawer as MUIDrawer,
+  Drawer,
   List,
-  ListItemButton,
+  useTheme,
+  Box,
+  Avatar,
+  Divider,
   ListItemText,
-  ListItem,
+  ListItemIcon,
+  ListItemButton,
 } from '@mui/material'
 
-import { grey } from '@mui/material/colors'
-
+import HomeIcon from '@mui/icons-material/Home'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import LightModeIcon from '@mui/icons-material/LightMode'
 import GroupIcon from '@mui/icons-material/Group'
 import LocalShippingIcon from '@mui/icons-material/LocalShipping'
 import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt'
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar'
+import { useThemeContext } from '@/contexts/ThemeContext'
+import ListItemLink from '../ListItemLink/ListItemLink'
 
 interface DrawerProps {
   isOpen: boolean
@@ -21,35 +28,80 @@ interface DrawerProps {
 }
 
 const DrawerAside = ({ isOpen, handleCloseDrawer }: DrawerProps) => {
+  const theme = useTheme()
+  const { toggleTheme, themeName } = useThemeContext()
+
   return (
-    <MUIDrawer
-      anchor="left"
-      open={isOpen}
-      onClose={handleCloseDrawer}
-      PaperProps={{ style: { background: grey[800], color: '#FFF' } }}
-    >
-      <List sx={{ width: 250 }}>
-        <ListItem sx={{ marginBottom: 3 }}>
-          <img src="/logo.png" alt="Imagem" width={'100%'} height={'100%'} />
-        </ListItem>
-        <ListItemButton onClick={handleCloseDrawer} sx={{ gap: 2 }}>
-          <GroupIcon />
-          <ListItemText primary="Clientes" />
-        </ListItemButton>
-        <ListItemButton onClick={handleCloseDrawer} sx={{ gap: 2 }}>
-          <LocalShippingIcon />
-          <ListItemText primary="Condutores" />
-        </ListItemButton>
-        <ListItemButton onClick={handleCloseDrawer} sx={{ gap: 2 }}>
-          <AddLocationAltIcon />
-          <ListItemText primary="Deslocamentos" />
-        </ListItemButton>
-        <ListItemButton onClick={handleCloseDrawer} sx={{ gap: 2 }}>
-          <DirectionsCarIcon />
-          <ListItemText primary="Veículos" />
-        </ListItemButton>
-      </List>
-    </MUIDrawer>
+    <Drawer variant="temporary" open={isOpen} onClose={handleCloseDrawer}>
+      <Box
+        width={theme.spacing(30)}
+        height="100%"
+        display="flex"
+        flexDirection="column"
+      >
+        <Box
+          width="100%"
+          height={theme.spacing(15)}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Avatar
+            sx={{
+              height: theme.spacing(10),
+              width: theme.spacing(10),
+            }}
+            src="/logo.png"
+            alt="Logo da página"
+          />
+        </Box>
+
+        <Divider />
+
+        <Box flex={1}>
+          <List>
+            <ListItemLink
+              text="Página inicial"
+              Icon={HomeIcon}
+              onclick={handleCloseDrawer}
+              to="/"
+            />
+            <ListItemLink
+              text="Clientes"
+              Icon={GroupIcon}
+              onclick={handleCloseDrawer}
+              to="/clientes"
+            />
+            <ListItemLink
+              text="Condutores"
+              Icon={LocalShippingIcon}
+              onclick={handleCloseDrawer}
+              to="/condutores"
+            />
+            <ListItemLink
+              text="Veículos"
+              Icon={DirectionsCarIcon}
+              onclick={handleCloseDrawer}
+              to="/veiculos"
+            />
+            <ListItemLink
+              text="Deslocamentos"
+              Icon={AddLocationAltIcon}
+              onclick={handleCloseDrawer}
+              to="/deslocamentos"
+            />
+          </List>
+        </Box>
+        <Box display="flex">
+          <ListItemButton onClick={toggleTheme}>
+            <ListItemIcon>
+              {themeName === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
+            </ListItemIcon>
+            <ListItemText>Alterar tema</ListItemText>
+          </ListItemButton>
+        </Box>
+      </Box>
+    </Drawer>
   )
 }
 
