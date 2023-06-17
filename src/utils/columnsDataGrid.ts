c2ed3131b7b8d0fa48d2a595f6766e373a22a817
Iarlen <iarlensr@hotmail.com>
2023-06-17 15:45:42 -0300

    refactor: adding documentType in clientColumns.

--- src/utils/columnsDataGrid.ts
+++ src/utils/columnsDataGrid.ts
@@ -1,10 +1,11 @@
 export const clientColumns = [
-  { field: 'id', headerName: 'ID', width: 100 },
-  { field: 'nome', headerName: 'Name', width: 170 },
-  { field: 'numeroDocumento', headerName: 'Número do Documento', width: 200 },
+  { field: 'id', headerName: 'ID', width: 80 },
+  { field: 'nome', headerName: 'Name', width: 150 },
+  { field: 'numeroDocumento', headerName: 'Número do Documento', width: 170 },
+  { field: 'tipoDocumento', headerName: 'Tipo', width: 80 },
   { field: 'cidade', headerName: 'Cidade', width: 150 },
   { field: 'uf', headerName: 'UF', width: 100 },
   { field: 'bairro', headerName: 'Bairro', width: 140 },
   { field: 'logradouro', headerName: 'Logradouro', width: 150 },
-  { field: 'numero', headerName: 'Número', width: 100 },
+  { field: 'numero', headerName: 'Número', width: 80 },
 ]
