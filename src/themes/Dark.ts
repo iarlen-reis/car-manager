ef851c4e857d2d3738657ce4d8dadfcaa3ba2ced
Iarlen <iarlensr@hotmail.com>
2023-06-17 01:09:36 -0300

    refactor: adding dark mode in dark theme color palette.

--- src/themes/Dark.ts
+++ src/themes/Dark.ts
@@ -3,6 +3,7 @@ import { lightBlue, deepOrange } from '@mui/material/colors'
 
 export const DarkTheme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
       main: deepOrange[700],
       dark: deepOrange[800],
