938b1ed81ef466fcec53db96880c88cfae377acc
Iarlen <iarlensr@hotmail.com>
2023-06-17 14:11:34 -0300

    refactor: creating columnsDataGrid file to store dataGrid columns.

---
+++ src/utils/columnsDataGrid.ts
@@ -0,0 +1,10 @@
+export const clientColumns = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  { field: 'nome', headerName: 'Name', width: 170 },
+  { field: 'numeroDocumento', headerName: 'Número do Documento', width: 200 },
+  { field: 'cidade', headerName: 'Cidade', width: 150 },
+  { field: 'uf', headerName: 'UF', width: 100 },
+  { field: 'bairro', headerName: 'Bairro', width: 140 },
+  { field: 'logradouro', headerName: 'Logradouro', width: 150 },
+  { field: 'numero', headerName: 'Número', width: 100 },
+]
