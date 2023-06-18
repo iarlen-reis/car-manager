b51f662195e345a4cb0ee18e92b19ab0b2061a04
Iarlen <iarlensr@hotmail.com>
2023-06-17 21:09:55 -0300

    refactor: adding provider from react query.

--- src/components/ContainerGlobal/ContainerGlobal.tsx
+++ src/components/ContainerGlobal/ContainerGlobal.tsx
@@ -1,13 +1,20 @@
 'use client'
 import React from 'react'
 import { Container } from '@mui/material'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 interface IContainerGlobalProps {
   children: React.ReactNode
 }
 
+const client = new QueryClient()
+
 const ContainerGlobal = ({ children }: IContainerGlobalProps) => {
-  return <Container maxWidth="xl">{children}</Container>
+  return (
+    <Container maxWidth="xl">
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+    </Container>
+  )
 }
 
 export default ContainerGlobal
