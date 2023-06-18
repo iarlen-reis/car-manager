21350b8b26f31b509ac9726fd759ad58035e7123
Iarlen <iarlensr@hotmail.com>
2023-06-17 21:08:05 -0300

    refactor: adding headers in instance axios.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,4 +2,8 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: 'https://api-deslocamento.herokuapp.com/api/v1',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 })
