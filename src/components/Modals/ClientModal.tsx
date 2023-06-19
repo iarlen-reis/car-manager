72860c1d0d763abb937d6d6264ce1a6ec2e4fc1c
Iarlen <iarlensr@hotmail.com>
2023-06-19 00:28:04 -0300

    refactor: starting to refactor the clientModel.

--- src/components/Modals/ClientModal.tsx
+++ src/components/Modals/ClientModal.tsx
@@ -142,131 +142,133 @@ const ClientModal = ({
         padding: 2,
       }}
     >
-      <FormProvider {...methods}>
-        <form
-          onSubmit={methods.handleSubmit(handleCreateClient)}
-          style={{
-            background: theme.palette.background.default,
-            padding: 20,
-            borderRadius: 5,
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 15,
-            maxWidth: 400,
-            width: '100%',
-            border: '1px solid',
-            borderColor: theme.palette.secondary.main,
-          }}
-        >
-          <Box
-            width="100%"
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
+      <div>
+        <FormProvider {...methods}>
+          <form
+            onSubmit={methods.handleSubmit(handleCreateClient)}
+            style={{
+              background: theme.palette.background.default,
+              padding: 20,
+              borderRadius: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 15,
+              maxWidth: 400,
+              width: '100%',
+              border: '1px solid',
+              borderColor: theme.palette.secondary.main,
+            }}
           >
-            <Typography fontSize={20} color={theme.palette.secondary.dark}>
-              Cadastrar cliente
-            </Typography>
-            <Button variant="text" onClick={handleCloseModal}>
-              <CloseIcon color="error" />
-            </Button>
-          </Box>
-          <Box width="100%" display="flex" flexDirection="column" gap={1.5}>
-            <FTextField
-              name="nome"
-              label="Nome"
-              rules={{ required: 'O campo é obrigatório.' }}
-            />
-            <FTwoTextFields
-              name="numeroDocumento"
-              name2="tipoDocumento"
-              label="Número do documento"
-              label2="Tipo"
-              width="70%"
-              width2="30%"
-              rules={{ required: 'O campo é obrigatório.' }}
-            />
-            <FTwoTextFields
-              name="cidade"
-              name2="uf"
-              label="Cidade"
-              label2="UF"
-              width="70%"
-              width2="30%"
-              rules={{ required: 'O campo é obrigatório.' }}
-            />
-            <FTwoTextFields
-              name="logradouro"
-              name2="numero"
-              label="Logradouro"
-              label2="Número"
-              width="70%"
-              width2="30%"
-              rules={{ required: 'O campo é obrigatório.' }}
-            />
-            <FTextField
-              name="bairro"
-              label="Bairro"
-              rules={{ required: 'O campo é obrigatório.' }}
-            />
-          </Box>
-          {!client ? (
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              disabled={!!client}
-              sx={{
-                height: '35px',
-              }}
+            <Box
+              width="100%"
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
             >
-              {createClient.isLoading ? (
-                <CircularProgress size={20} color="secondary" />
-              ) : (
-                <Typography
-                  color={theme.palette.primary.light}
-                  fontWeight={600}
-                  sx={{ fontSize }}
-                >
-                  Adicionar
-                </Typography>
-              )}
-            </Button>
-          ) : (
-            <Box width="100%" display="flex" alignItems="center" gap={1}>
-              <Button
-                onClick={handleDeleteClient}
-                variant="contained"
-                color="error"
-                sx={{ width: '50%', minHeight: '35px' }}
-              >
-                <Typography
-                  color={theme.palette.primary.light}
-                  fontWeight={600}
-                  sx={{ fontSize }}
-                >
-                  Deletar
-                </Typography>
+              <Typography fontSize={20} color={theme.palette.secondary.dark}>
+                Cadastrar cliente
+              </Typography>
+              <Button variant="text" onClick={handleCloseModal}>
+                <CloseIcon color="error" />
               </Button>
+            </Box>
+            <Box width="100%" display="flex" flexDirection="column" gap={1.5}>
+              <FTextField
+                name="nome"
+                label="Nome"
+                rules={{ required: 'O campo é obrigatório.' }}
+              />
+              <FTwoTextFields
+                name="numeroDocumento"
+                name2="tipoDocumento"
+                label="Número do documento"
+                label2="Tipo"
+                width="70%"
+                width2="30%"
+                rules={{ required: 'O campo é obrigatório.' }}
+              />
+              <FTwoTextFields
+                name="cidade"
+                name2="uf"
+                label="Cidade"
+                label2="UF"
+                width="70%"
+                width2="30%"
+                rules={{ required: 'O campo é obrigatório.' }}
+              />
+              <FTwoTextFields
+                name="logradouro"
+                name2="numero"
+                label="Logradouro"
+                label2="Número"
+                width="70%"
+                width2="30%"
+                rules={{ required: 'O campo é obrigatório.' }}
+              />
+              <FTextField
+                name="bairro"
+                label="Bairro"
+                rules={{ required: 'O campo é obrigatório.' }}
+              />
+            </Box>
+            {!client ? (
               <Button
                 type="submit"
                 variant="contained"
                 color="primary"
-                fullWidth
-                sx={{ minHeight: '35px' }}
+                disabled={!!client}
+                sx={{
+                  height: '35px',
+                }}
               >
-                <Typography
-                  color={theme.palette.primary.light}
-                  fontWeight={600}
-                  sx={{ fontSize }}
-                >
-                  Editar
-                </Typography>
+                {createClient.isLoading ? (
+                  <CircularProgress size={20} color="secondary" />
+                ) : (
+                  <Typography
+                    color={theme.palette.primary.light}
+                    fontWeight={600}
+                    sx={{ fontSize }}
+                  >
+                    Adicionar
+                  </Typography>
+                )}
               </Button>
-            </Box>
-          )}
-        </form>
-      </FormProvider>
+            ) : (
+              <Box width="100%" display="flex" alignItems="center" gap={1}>
+                <Button
+                  onClick={handleDeleteClient}
+                  variant="contained"
+                  color="error"
+                  sx={{ width: '50%', minHeight: '35px' }}
+                >
+                  <Typography
+                    color={theme.palette.primary.light}
+                    fontWeight={600}
+                    sx={{ fontSize }}
+                  >
+                    Deletar
+                  </Typography>
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  sx={{ minHeight: '35px' }}
+                >
+                  <Typography
+                    color={theme.palette.primary.light}
+                    fontWeight={600}
+                    sx={{ fontSize }}
+                  >
+                    Editar
+                  </Typography>
+                </Button>
+              </Box>
+            )}
+          </form>
+        </FormProvider>
+      </div>
     </Modal>
   )
 }
