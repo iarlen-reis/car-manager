b5a01c534542e60463062d9b2fd1cfbb574c3a47
Iarlen <iarlensr@hotmail.com>
2023-06-19 22:29:52 -0300

    refactor: removing requests from the Condutores page.

--- src/app/condutores/page.tsx
+++ src/app/condutores/page.tsx
@@ -2,132 +2,25 @@
 import React, { useState } from 'react'
 import { Box } from '@mui/material'
 import MenuTools from '@/components/MenuTools/MenuTools'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { api } from '@/utils/api'
-import { formateDate } from '@/utils/formatDate'
 import useColumns from '@/hooks/useColumns'
 import DataGridTable from '@/components/DataGridTable/DataGridTable'
 import DriverModal from '@/components/Modals/DriverModal'
-
-import { IDriverProps } from '@/@types/modals/driverModalTypes'
+import { useDrivers } from '@/hooks/useDrivers'
 
 const Condutores = () => {
+  const {
+    drivers,
+    driversLoading,
+    driver,
+    searchDriver,
+    setDriver,
+    createDriver,
+    updateDriver,
+    deleteDriver,
+  } = useDrivers()
   const { driverColumns } = useColumns()
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
-  const [driver, setDriver] = useState<IDriverProps | null>(null)
-  const queryClient = useQueryClient()
 
-  // get all driver: busca todos condutores
-  const { data: drivers, isLoading } = useQuery(['drivers'], async () => {
-    const response = await api.get<IDriverProps[]>('/condutor')
-
-    response.data.map(
-      (driver) =>
-        (driver.vencimentoHabilitacao = formateDate(
-          driver.vencimentoHabilitacao,
-        )),
-    )
-
-    return response.data
-  })
-
-  // create a new driver: Criar um novo condutor
-  const { mutate: createDriver } = useMutation(
-    (driver: IDriverProps) => api.post('/Condutor', driver),
-    {
-      onSuccess: (data) => {
-        const driver = JSON.parse(data.config.data) as IDriverProps
-
-        driver.vencimentoHabilitacao = formateDate(driver.vencimentoHabilitacao)
-
-        const driverOlds = queryClient.getQueryData<IDriverProps[]>(['drivers'])
-
-        if (driverOlds) {
-          const newClients = [
-            ...driverOlds,
-            {
-              ...driver,
-              id: driverOlds.length,
-              catergoriaHabilitacao: driver.categoriaHabilitacao,
-            },
-          ]
-
-          queryClient.setQueryData(['drivers'], newClients)
-        }
-      },
-    },
-  )
-
-  // delete a driver: Deleta um condutor
-  const { mutate: deleteDriver } = useMutation(
-    (id: number) =>
-      api.delete(`/condutor/${id}`, {
-        data: {
-          id,
-        },
-      }),
-    {
-      onSuccess: (data) => {
-        const parsedData = JSON.parse(data.config.data)
-        const id = parsedData.id
-
-        const oldDrivers = queryClient.getQueryData<IDriverProps[]>(['drivers'])
-
-        const newDrivers = oldDrivers?.filter((driver) => driver.id !== id)
-
-        queryClient.setQueryData(['drivers'], newDrivers)
-
-        setIsOpenModal(false)
-      },
-    },
-  )
-
-  // get a driver: Buscando um condutor
-  const { mutate: searchDriver } = useMutation(
-    (id: number) => api.get<IDriverProps>(`/condutor/${id}`),
-    {
-      onSuccess: (data) => {
-        if (data.data) {
-          setDriver(data.data)
-          setIsOpenModal(true)
-        }
-      },
-    },
-  )
-
-  // update a driver: Atualiza um condutor
-  const { mutate: updateDriver } = useMutation(
-    (
-      driverUpdated: Pick<
-        IDriverProps,
-        'id' | 'categoriaHabilitacao' | 'vencimentoHabilitacao'
-      >,
-    ) => api.put(`/condutor/${driverUpdated.id}`, driverUpdated),
-    {
-      onSuccess: (data) => {
-        const driverUpdated = JSON.parse(data.config.data) as IDriverProps
-
-        const oldDrivers = queryClient.getQueryData<IDriverProps[]>(['drivers'])
-
-        const newDrivers = oldDrivers?.map((driver) => {
-          if (driver.id === driverUpdated.id) {
-            driver.categoriaHabilitacao = driverUpdated.categoriaHabilitacao
-            driver.vencimentoHabilitacao = formateDate(
-              driverUpdated.vencimentoHabilitacao,
-            )
-          }
-          return driver
-        })
-        queryClient.setQueryData(['drivers'], newDrivers)
-        setIsOpenModal(false)
-      },
-    },
-  )
-
-  const handleShow = () => {
-    setIsOpenModal(!isOpenModal)
-    setDriver(null)
-  }
   const handleOpenModal = () => {
     setDriver(null)
     setIsOpenModal(!isOpenModal)
@@ -138,18 +31,18 @@ const Condutores = () => {
       <MenuTools
         description="Página de condutores"
         textButton="novo condutor"
-        handleOpenModal={handleShow}
+        handleOpenModal={handleOpenModal}
       />
       <DataGridTable
         columns={driverColumns}
         rows={drivers}
-        loading={isLoading}
+        loading={driversLoading}
         handleShow={searchDriver}
         handleDelete={deleteDriver}
         handleOpenModal={handleOpenModal}
       />
       <DriverModal
-        handleModal={handleShow}
+        handleModal={handleOpenModal}
         isOpen={isOpenModal}
         deleteDriver={deleteDriver}
         createDriver={createDriver}
