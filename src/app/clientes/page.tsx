c8fc9dab138dd93ca3a616f3fc00b2cb15bd080e
Iarlen <iarlensr@hotmail.com>
2023-06-19 22:11:03 -0300

    refactor: refactoring Clientes page.

--- src/app/clientes/page.tsx
+++ src/app/clientes/page.tsx
@@ -4,121 +4,29 @@ import { Box } from '@mui/material'
 import MenuTools from '@/components/MenuTools/MenuTools'
 import ClientModal from '@/components/Modals/ClientModal'
 import DataGridTable from '@/components/DataGridTable/DataGridTable'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { api } from '@/utils/api'
 import useColumns from '@/hooks/useColumns'
-
-import { IClientProps } from '@/@types/modals/clientModalTypes'
+import { useClients } from '@/hooks/useClients'
 
 const Cliente = () => {
   const [openModal, setOpenModal] = useState(false)
-  const [clientSearchData, setClientSearchData] = useState<IClientProps | null>(
-    null,
-  )
+  const {
+    clientes,
+    clientsLoading,
+    createClient,
+    isLoadingCreate,
+    updateClient,
+    deleteClient,
+    client,
+    clientSearch,
+    setClient,
+  } = useClients()
+
   const { clientColumns } = useColumns()
 
-  const queryClient = useQueryClient()
-
-  // get all clients: Busca todos clientes
-  const { data: clientes, isFetching } = useQuery(
-    ['clients'],
-    async () => {
-      const response = await api.get<IClientProps[] | undefined>('/cliente')
-
-      return response.data
-    },
-    {
-      staleTime: 1000 * 60,
-      refetchOnWindowFocus: true,
-    },
-  )
-
-  // create a new client: Cria um novo cliente
-  const { mutate: createClient, isLoading: isLoadingCreate } = useMutation(
-    (client: IClientProps) => api.post('/cliente', client),
-    {
-      onSuccess: (data, variables) => {
-        const client = JSON.parse(data.config.data)
-
-        const clientOlds = queryClient.getQueryData<IClientProps[]>(['clients'])
-
-        if (clientOlds) {
-          const newClients = [
-            ...clientOlds,
-            { ...client, id: clientOlds.length },
-          ]
-
-          queryClient.setQueryData(['clients'], newClients)
-        }
-      },
-    },
-  )
-
-  // update a client: Atualiza um cliente.
-  const { mutate: updateClient } = useMutation(
-    (updatedClient: IClientProps) =>
-      api.put(`/cliente/${updatedClient.id}`, updatedClient),
-    {
-      onSuccess: (data, updatedClient) => {
-        const clientUpdated = JSON.parse(data.config.data)
-
-        queryClient.setQueryData<IClientProps[] | undefined>(
-          ['clients'],
-          (oldData) => {
-            if (oldData) {
-              return oldData.map((client: IClientProps) =>
-                client.id === clientUpdated.id ? clientUpdated : client,
-              )
-            }
-            return oldData
-          },
-        )
-      },
-    },
-  )
-
-  // delete a client: Deleta um cliente
-  const { mutate: deleteClient } = useMutation(
-    (id: number) => {
-      return api.delete(`Cliente/${id}`, {
-        data: { id },
-      })
-    },
-    {
-      onSuccess: (data, variables) => {
-        const parsedData = JSON.parse(data.config.data)
-        const id = parsedData.id
-        const oldClients = queryClient.getQueryData<IClientProps[]>(['clients'])
-
-        if (oldClients) {
-          const newClients = oldClients?.filter((client) => client.id !== id)
-
-          queryClient.setQueryData(['clients'], newClients)
-        }
-      },
-    },
-  )
-
-  // get a client: busca um cliente
-  const { mutate: clientSearch } = useMutation(
-    (id: number) => {
-      return api.get<IClientProps>(`/cliente/${id}`)
-    },
-    {
-      onSuccess: (data) => {
-        if (data.data) {
-          setClientSearchData(data.data)
-
-          setOpenModal(true)
-        } else {
-          setClientSearchData(null)
-        }
-      },
-    },
-  )
+  console.log('oieee')
 
   const handleOpenModal = () => {
-    setClientSearchData(null)
+    setClient(null)
     setOpenModal(!openModal)
   }
 
@@ -136,14 +44,14 @@ const Cliente = () => {
         handleDelete={deleteClient}
         columns={clientColumns}
         rows={clientes}
-        loading={isFetching}
+        loading={clientsLoading}
       />
 
       <ClientModal
         isOpen={openModal}
         isLoadingCreate={isLoadingCreate}
         handleModal={handleOpenModal}
-        client={clientSearchData}
+        client={client}
         createClient={createClient}
         updateClient={updateClient}
         deleteClient={deleteClient}
