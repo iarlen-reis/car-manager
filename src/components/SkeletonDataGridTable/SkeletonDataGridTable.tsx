3f5350e6ceacbee46c69ef0fa37cbd1a3091bfe6
Iarlen <iarlensr@hotmail.com>
2023-06-22 18:10:01 -0300

    feat: create a component of Skeleton to DataGridTable.

---
+++ src/components/SkeletonDataGridTable/SkeletonDataGridTable.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { Box, Skeleton } from '@mui/material'
+import React from 'react'
+
+const SkeletonDataGridTable = () => {
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="flex"
+      flexDirection="column"
+      gap={1}
+      padding={1}
+    >
+      <Box width="100%" display="flex" gap={2} padding={0} margin={0}>
+        <Skeleton width="100%" height={40} animation="wave" variant="rounded" />
+        <Skeleton width="100%" height={40} animation="wave" variant="rounded" />
+        <Skeleton width="100%" height={40} animation="wave" variant="rounded" />
+        <Skeleton width="100%" height={40} animation="wave" variant="rounded" />
+      </Box>
+      <Box flex={1} width="100%" display="flex">
+        <Skeleton
+          width="100%"
+          height="100%"
+          animation="wave"
+          variant="rounded"
+        />
+      </Box>
+      <Box width="100%" display="flex">
+        <Skeleton width="100%" height={40} animation="wave" variant="rounded" />
+      </Box>
+    </Box>
+  )
+}
+
+export default SkeletonDataGridTable
