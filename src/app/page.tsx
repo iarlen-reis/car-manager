57364008226761de83e42f9f9dde8741fb8789c3
Iarlen <iarlensr@hotmail.com>
2023-06-25 01:22:20 -0300

    refactor: changing the structure of the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,22 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="mt-4 flex min-h-screen w-full flex-col gap-3 lg:mt-11 lg:grid lg:grid-cols-2 lg:gap-2">
-      <section>
-        <div className="flex flex-col gap-2">
-          <h1 className="font-itim text-4xl text-emerald-500 lg:text-5xl">
-            Car Mananger
+    <div className="flex min-h-screen w-full flex-col gap-5 lg:mt-11 lg:grid lg:grid-cols-2 lg:gap-2">
+      <section className="lg:mt-8">
+        <div className="flex flex-col gap-2 text-center lg:text-start xl:gap-3">
+          <h1 className="xl:7xl font-ysabeau text-4xl font-bold tracking-wider text-emerald-700 sm:text-5xl lg:text-6xl">
+            <span className="text-6xl sm:text-7xl xl:text-8xl">C</span>ar{' '}
+            <span className="text-6xl sm:text-7xl xl:text-8xl">M</span>ananger
           </h1>
-          <p className="mt-2 max-w-[500px] text-base italic text-emerald-400 md:max-w-[700px] lg:w-full lg:text-xl">
-            Gerencie seus <span className="font-bold">clientes</span>,{' '}
-            <span className="font-bold">condutores</span> e{' '}
-            <span className="font-bold">veículos</span> de forma fácil e
-            simples! Escolha o veículo e o condutor e comece o deslocamento!
-          </p>
-          <p className="mt-2 max-w-[500px] text-base italic text-emerald-400 md:max-w-[700px] lg:w-full lg:text-xl">
-            Além disso, também é possivel gerenciar o{' '}
-            <span className="font-bold">deslocamento</span> dos veículos,
-            podendo verificar o inicio de um deslocamento e seu final.
-          </p>
+          <div className="flex flex-col items-center justify-center gap-1 lg:items-start lg:justify-start">
+            <p className="xl:4xl max-w-[420px] font-ysabeau text-2xl font-semibold tracking-wider text-emerald-600 sm:text-3xl">
+              Gerenciamento de clientes, codutores e veículos.
+            </p>
+            <p className="font-poppins text-base italic text-emerald-700 sm:text-xl xl:text-2xl">
+              de forma <span className="italic underline">fácil</span> e{' '}
+              <span className="italic underline">eficiência.</span>
+            </p>
+          </div>
         </div>
       </section>
       <section className="w-full">
