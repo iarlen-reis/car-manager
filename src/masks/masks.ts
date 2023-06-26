d35173788f806b883fa036f369c31294caf3c346
Iarlen <iarlensr@hotmail.com>
2023-06-26 20:17:29 -0300

    refactor: modify regex of removeSpecialCharacters.

--- src/masks/masks.ts
+++ src/masks/masks.ts
@@ -8,10 +8,10 @@ export const normalizePhoneNumber = (value: string | undefined) => {
     .replace(/(-\d{4})(\d+?)/, '$1')
 }
 
-export const removeSpecialCharacters = (value) => {
+export const removeSpecialCharacters = (value: string | undefined) => {
   if (!value) return ''
 
-  return value.replace(/[\/\.\-]/g, '')
+  return value.replace(/[\\/\\.\\-]/g, '')
 }
 
 export const normalizeCnpjNumber = (value: string | undefined) => {
