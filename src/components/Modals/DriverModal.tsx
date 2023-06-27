98a4e9bc152efe54b804672244bbc8cdd9c8d2ad
Iarlen <iarlensr@hotmail.com>
2023-06-26 21:17:28 -0300

    refactor: adding mascara in the License Number input.

--- src/components/Modals/DriverModal.tsx
+++ src/components/Modals/DriverModal.tsx
@@ -11,7 +11,6 @@ import {
 import CloseIcon from '@mui/icons-material/Close'
 
 import FTextField from '../FTextField/FTextField'
-import FTwoTextFields from '../FTextField/FTwoTextFields'
 import { FormProvider, useForm } from 'react-hook-form'
 import FTextFieldDate from '../FTextField/FTextFieldDate'
 import { backToStringDate, formateDate } from '@/utils/formatDate'
@@ -22,6 +21,8 @@ import {
 } from '@/@types/modals/driverModalTypes'
 import { toast } from 'react-toastify'
 import { Add, DeleteForever, Edit } from '@mui/icons-material'
+import { normalizeCnhNumber } from '@/masks/masks'
+import FSelectField from '../FTextField/FSelectField'
 
 const DriverModal = ({
   isOpen,
@@ -34,10 +35,52 @@ const DriverModal = ({
   const methods = useForm<IDriverProps>()
   const theme = useTheme()
 
+  const licenseCategory = methods.watch('catergoriaHabilitacao')
+  const licenseDocument = methods.watch('numeroHabilitacao')
+
   const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
   const isMedium = useMediaQuery(theme.breakpoints.down('md'))
 
   const fontSize = isSmall ? '14px' : isMedium ? '16px' : '18px'
+
+  const optionsLicenseCategory = [
+    {
+      id: 'a',
+      nome: 'A',
+    },
+    {
+      id: 'b',
+      nome: 'C',
+    },
+    {
+      id: 'c',
+      nome: 'C',
+    },
+    {
+      id: 'd',
+      nome: 'D',
+    },
+    {
+      id: 'e',
+      nome: 'E',
+    },
+    {
+      id: 'ab',
+      nome: 'AB',
+    },
+    {
+      id: 'ac',
+      nome: 'AC',
+    },
+    {
+      id: 'ad',
+      nome: 'AD',
+    },
+    {
+      id: 'ae',
+      nome: 'AE',
+    },
+  ]
 
   const handleCloseAndClearFields = () => {
     methods.reset()
@@ -90,6 +133,12 @@ const DriverModal = ({
       handleModal()
     }
   }
+
+  useEffect(() => {
+    if (licenseDocument && licenseDocument.length !== 0) {
+      methods.setValue('numeroHabilitacao', normalizeCnhNumber(licenseDocument))
+    }
+  }, [licenseDocument, licenseCategory])
 
   useEffect(() => {
     if (driver?.id && driver.catergoriaHabilitacao) {
@@ -152,16 +201,21 @@ const DriverModal = ({
               rules={{ required: 'O campo é obrigatório.' }}
               disabled={!!driver}
             />
-            <FTwoTextFields
-              name="numeroHabilitacao"
-              name2="categoriaHabilitacao"
-              label="Número da Habilitação"
-              label2="Categoria"
-              width="70%"
-              width2="30%"
-              rules={{ required: 'O campo é obrigatório.' }}
-              disabled={!!driver}
-            />
+            <div className="flex w-full items-center gap-1">
+              <div className="w-1/3">
+                <FSelectField
+                  label="Categoria"
+                  name="categoriaHabilitacao"
+                  options={optionsLicenseCategory}
+                />
+              </div>
+              <div className="w-full">
+                <FTextField
+                  label="Número Habilitação"
+                  name="numeroHabilitacao"
+                />
+              </div>
+            </div>
             <Box display="flex" flexDirection="column">
               <Typography fontSize="10px" variant="button" color="#9e9e9e">
                 Vencimento
