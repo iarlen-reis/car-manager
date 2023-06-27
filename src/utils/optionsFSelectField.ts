8ffe09b023c3f3731d1da375f5475f7e938bbc7f
Iarlen <iarlensr@hotmail.com>
2023-06-27 19:56:25 -0300

    feat: adding file optionsFSelectorFields.

---
+++ src/utils/optionsFSelectField.ts
@@ -0,0 +1,57 @@
+export const optionsTypeDocument = [
+  {
+    id: 'cpf',
+    nome: 'CPF',
+  },
+  {
+    id: 'cnpj',
+    nome: 'CNPJ',
+  },
+  {
+    id: 'rg',
+    nome: 'RG',
+  },
+  {
+    id: 'cnh',
+    nome: 'CNH',
+  },
+]
+
+export const optionsLicenseCategory = [
+  {
+    id: 'a',
+    nome: 'A',
+  },
+  {
+    id: 'b',
+    nome: 'B',
+  },
+  {
+    id: 'c',
+    nome: 'C',
+  },
+  {
+    id: 'd',
+    nome: 'D',
+  },
+  {
+    id: 'e',
+    nome: 'E',
+  },
+  {
+    id: 'ab',
+    nome: 'AB',
+  },
+  {
+    id: 'ac',
+    nome: 'AC',
+  },
+  {
+    id: 'ad',
+    nome: 'AD',
+  },
+  {
+    id: 'ae',
+    nome: 'AE',
+  },
+]
